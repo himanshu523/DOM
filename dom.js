805a87bf59f1b2b25a0b1552f73b38d20c257aec
himanshu523 <himanshuranjan.y@outlook.com>
2022-10-27 23:49:09 +0530

    dom manipulation

---
+++ dom.js
@@ -0,0 +1,20 @@
+console.log(document.body)
+console.log(document.title)
+document.querySelector('title').innerText="DOM MANIPULATION";
+console.log(document.all)
+console.log(document.all[5])
+
+
+const headertitle= document.getElementById('header-title');
+console.log(headertitle);
+
+//GETELEMENTBYID
+
+console.log(document.getElementById('header-title'));
+var headerTitle = document.getElementById('header-title');
+var header =document.getElementById('main-header');
+console.log(headertitle);
+headerTitle.textContent ='hello';
+headerTitle.innerText='good bye';
+headerTitle.innerHTML='<h3>helloo</h3>';
+header.style.borderBottom='solid 3px #000';
