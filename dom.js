7cf45b57b20145ef8e1e87a2b3f93285c8425b27
himanshu523 <himanshuranjan.y@outlook.com>
2022-10-28 20:50:27 +0530

    added for loop to manipulate with class

--- dom.js
+++ dom.js
@@ -29,8 +29,12 @@ item.style.color ="green";
 
 var item = document.getElementsByClassName('list-group-item');
 console.log(item);
-item[0].style.fontWeight='bold';
-item[1].style.fontWeight='bold';
-item[2].style.fontWeight='bold';
-item[3].style.fontWeight='bold';
+//item[0].style.fontWeight='bold';
+//item[1].style.fontWeight='bold';
+//item[2].style.fontWeight='bold';
+//item[3].style.fontWeight='bold';
 item[2].style.backgroundColor='green';
+for(var i=0;i<item.length;i++)
+{
+    item[i].style.fontWeight='bold';
+}
