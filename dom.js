bf2607b34e5d57f47d74332f44d4963bc8a48110
himanshu523 <himanshuranjan.y@outlook.com>
2022-10-29 11:37:27 +0530

    added QUERYSELECTOR

--- dom.js
+++ dom.js
@@ -1,46 +1,64 @@
-console.log(document.body)
-console.log(document.title)
-document.querySelector('title').innerText="DOM MANIPULATION";
-console.log(document.all)
-console.log(document.all[5])
+//console.log(document.body)
+//console.log(document.title)
+//document.querySelector('title').innerText="DOM MANIPULATION";
+//console.log(document.all)
+//console.log(document.all[5])
 
 
-const headertitle= document.getElementById('header-title');
-console.log(headertitle);
+//const headertitle= document.getElementById('header-title');
+//console.log(headertitle);
 
 //GETELEMENTBYID
 
-console.log(document.getElementById('header-title'));
-var headerTitle = document.getElementById('header-title');
-var header =document.getElementById('main-header');
-console.log(headertitle);
-headerTitle.textContent ='hello';
-headerTitle.innerText='good bye';
-headerTitle.innerHTML='<h3>helloo</h3>';
-header.style.borderBottom='solid 3px #000';
+//console.log(document.getElementById('header-title'));
+//var headerTitle = document.getElementById('header-title');
+//var header =document.getElementById('main-header');
+//console.log(headertitle);
+//headerTitle.textContent ='hello';
+//headerTitle.innerText='good bye';
+//headerTitle.innerHTML='<h3>helloo</h3>';
+//header.style.borderBottom='solid 3px #000';
 
 //changing additem to bold and green
 
-var item =document.querySelector('.title');
-item.style.fontWeight ="bold";
-item.style.color ="green";
+//var item =document.querySelector('.title');
+//item.style.fontWeight ="bold";
+//item.style.color ="green";
 
 //GETELEMENTBYCLASS
 
-var item = document.getElementsByClassName('list-group-item');
-console.log(item);
+//var item = document.getElementsByClassName('list-group-item');
+//console.log(item);
 //item[0].style.fontWeight='bold';
 //item[1].style.fontWeight='bold';
 //item[2].style.fontWeight='bold';
 //item[3].style.fontWeight='bold';
-item[2].style.backgroundColor='green';
-for(var i=0;i<item.length;i++)
-{
-    item[i].style.fontWeight='bold';
-}
+//item[2].style.backgroundColor='green';
+//for(var i=0;i<item.length;i++)
+//{
+//    item[i].style.fontWeight='bold';
+//}
 
 //GETELEMENTBYTAGNAME
 
-var li =document.getElementsByTagName('li')
-console.log(li);
-console.log(li[4]);
+//var li =document.getElementsByTagName('li')
+//console.log(li);
+//console.log(li[4]);
+
+//QUERYSELECTOR
+
+//var header = document.querySelector('#main-header');
+//header.style.borderBottom ='solid 4px #ccc';
+
+
+//var input = document.querySelector('input');
+//input.value="value";
+
+//var submit = document.querySelector('input[type="submit"]');
+//submit.value ="send";
+
+//var lastitem =document.querySelector('.list-group-item:last-child');
+//lastitem.style.color='blue';
+
+//var seconditem = document.querySelector('.list-group-item:nth-child(2)');
+//seconditem.style.color ='coral';
